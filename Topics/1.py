e0b8c8f2c22b7f61a627682e5fbd121a928fd6a2
Nitesh <127967694+Nitesh8750@users.noreply.github.com>
2026-04-15 23:42:26 +0530

    Add files via upload

---
+++ Topics/1.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+a = input("Enter a:")
+b = input("Enter b:")
+separator = input("Enter Sep:")[0]
+
+########### Write your code below ###############
+
+# Print with space
+print(a,end=" ")
+print(b)
+
+# Print without newline at the end
+print(a,end=" ")
+print(b,end="")
+
+# Print with separator
+print(a,end=separator)
+print(b)
+
+# Print without space
+print(a,end="")
+print(b)
+
+########### Write your code above ###############
